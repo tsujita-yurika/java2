42c4e77c593d272f703b5aa51cac1dc862a0384d
tsujita-yurika <kotoraneko11@gmail.com>
2023-12-28 15:44:20 +0900

    P104,105,106制御構文（条件式とブロック）

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -4,13 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		boolean tenki = true;
-//		天気は晴れか
-		if (tenki == true) {
-			System.out.println("洗濯をします");
-			System.out.println("散歩に行きます");
-		} else {
-		System.out.println("DVDを観ます");
+		boolean doorClose = false;
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("１分待つ");
 		
 		}
 
