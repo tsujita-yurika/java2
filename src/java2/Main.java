a6c6abcb28bf189dac136fd7b267743c2679a796
tsujita-yurika <kotoraneko11@gmail.com>
2023-12-31 14:08:50 +0900

    調整

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+//		8行目がtrueの場合ノックする、1分待つが繰り返される
 		boolean doorClose = false;
 		while (doorClose == true) {
 			System.out.println("ノックする");
