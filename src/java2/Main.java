16225fde75bad8dddb3543ad1e6b02e124cf719a
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-05 15:02:11 +0900

    ループ変数を10から1ずつ1まで減らす

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 //		複雑なfor文
-//		ループ変数を２ずつ増やす
-		for (int i = 0; i < 10; i += 2) {
+//		ループ変数を10から1ずつ1まで減らす
+		for (int i = 10; i > 0; i--) {
 			System.out.println("現在" + i + "周目→");
 			
 			}
