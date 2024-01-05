e54f97fff887ade90a250a0970d09fe4595edd01
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-05 14:42:16 +0900

    ループ変数

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-//		for文で10回繰り返す
-		for (int i = 0; i < 10; i++) {
-			System.out.println("こんにちは");
+//		ループ変数の名前は自由
+//		ブロック内で利用可能
+		for (int i = 0; i < 3; i++) {
+			System.out.println("現在" + (i + 1) + "周目→");
+
 			
 			}
 		
