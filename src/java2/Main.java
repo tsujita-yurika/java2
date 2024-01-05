1071459f992754edb8ed7c05884711684adfff93
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-05 14:59:02 +0900

    ループ変数を1からスタートする

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -4,11 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-//		ループ変数の名前は自由
-//		ブロック内で利用可能
-		for (int i = 0; i < 3; i++) {
-			System.out.println("現在" + (i + 1) + "周目→");
-
+//		複雑なfor文
+//		ループ変数を1からスタートする
+		for (int i = 1; i < 10; i++) {
+			System.out.println("現在" + i + "周目→");
 			
 			}
 		
