9ab6f89b5e9d5331d83a11f69aba4a45146df31e
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-05 15:18:11 +0900

    繰り返し時の処理を行わない

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -5,8 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 //		複雑なfor文
-//		ループ変数を初期化しないため、コンパイルエラーが発生？
-		for (; i < 10; i++) {
+//		繰り返し時の処理を行わない
+//		ずっと0周目→
+		for (int i = 0; i < 10;) {
 			System.out.println("現在" + i + "周目→");
 			
 			}
