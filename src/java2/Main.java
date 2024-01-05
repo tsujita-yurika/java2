214b2f45cc06bb839daf7b7656101d9a1c3eccbe
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-05 15:00:07 +0900

    ループ変数を２ずつ増やす

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 //		複雑なfor文
-//		ループ変数を1からスタートする
-		for (int i = 1; i < 10; i++) {
+//		ループ変数を２ずつ増やす
+		for (int i = 0; i < 10; i += 2) {
 			System.out.println("現在" + i + "周目→");
 			
 			}
