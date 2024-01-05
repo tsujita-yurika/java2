1234806ea1e9c38de40cce02beb0ab6f22f012d2
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-05 14:38:24 +0900

    回数を指定して繰り返す

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -4,19 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("あなたの運勢を占います");
-		int fortune = new java.util.Random().nextInt(5) + 1;
-		switch (fortune) {
-		case 1:
-		case 2:
-			System.out.println("いいね！");
-			break;
-		case 3:
-			System.out.println("普通です");
-			break;
-		case 4:
-		case 5:
-			System.out.println("うーん・・・");
+//		for文で10回繰り返す
+		for (int i = 0; i < 10; i++) {
+			System.out.println("こんにちは");
 			
 			}
 		
