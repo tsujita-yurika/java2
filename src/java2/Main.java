27c6495e10481e58f2df0a0cc07b9e4cae32cb74
tsujita-yurika <kotoraneko11@gmail.com>
2024-01-12 23:54:17 +0900

    1〜10までの数字のうち偶数のみを昇順で表示させる

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -1,14 +1,18 @@
 package java2;
 
 public class Main {
-
 	public static void main(String[] args) {
-        for ( int i = 1; i <= 30; i++ ) {
-            if ( i % 3 == 0 && i % 5 == 0 ) System.out.print("FizzBuzz");
-            else if ( i % 3 == 0 ) System.out.print("Fizz");
-            else if ( i % 5 == 0 ) System.out.print("Buzz");
-            else System.out.print(i);
-        	}
+//		初期化、繰り返し条件、処理
+		for(int i=1; i<=10; i++){
+//			iが偶数である場合
+			if(i % 2 == 0){
+//				2 4 6 8 10と繰り返し出力されるiにスペースを入れる
+//				printlnにすると出力が縦書きになる
+				System.out.print(i+" ");
+				}
+			}
+//		空の行を出力する。無くてもここでは結果は同じ
+		System.out.println();
 		}
 	}
 	
